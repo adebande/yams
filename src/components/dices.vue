d8c27e0fc9120e3e60e2db85658f5ca1efcf61f9
Aloïs Debande <63793809+adebande@users.noreply.github.com>
2020-04-27 01:30:04 +0200

    add dice selection
    Dices can be selected by clicking on image (no more checkboxes).
    Html page title changed to "Yam's"

--- src/components/dices.vue
+++ src/components/dices.vue
@@ -3,16 +3,11 @@
         <p>Player {{player}}</p>
         <p>{{tries}} {{triesText}} left.</p>
         <div id="dicesImages">
-            <img :src="dices[0].diceImg">
-            <input type="checkbox" v-model="dices[0].keep">
-            <img :src="dices[1].diceImg">
-            <input type="checkbox" v-model="dices[1].keep">
-            <img :src="dices[2].diceImg">
-            <input type="checkbox" v-model="dices[2].keep">
-            <img :src="dices[3].diceImg">
-            <input type="checkbox" v-model="dices[3].keep">
-            <img :src="dices[4].diceImg">
-            <input type="checkbox" v-model="dices[4].keep">
+            <img :src="dices[0].diceImg" :class="dices[0].activeDice" @click="keepDice(dices[0])">
+            <img :src="dices[1].diceImg" :class="dices[1].activeDice" @click="keepDice(dices[1])">
+            <img :src="dices[2].diceImg" :class="dices[2].activeDice" @click="keepDice(dices[2])">
+            <img :src="dices[3].diceImg" :class="dices[3].activeDice" @click="keepDice(dices[3])">
+            <img :src="dices[4].diceImg" :class="dices[4].activeDice" @click="keepDice(dices[4])">
         </div>
         <div id="rollButton">
             <b-button class="buttons" @click="roll" :disabled="disableRoll">Roll</b-button>
@@ -28,11 +23,11 @@
         data() {
             return {
                 dices: [
-                    {id: 0, diceValue: 0, diceImg: require('../assets/diceempty.png'), keep: false},
-                    {id: 1, diceValue: 0, diceImg: require('../assets/diceempty.png'), keep: false},
-                    {id: 2, diceValue: 0, diceImg: require('../assets/diceempty.png'), keep: false},
-                    {id: 3, diceValue: 0, diceImg: require('../assets/diceempty.png'), keep: false},
-                    {id: 4, diceValue: 0, diceImg: require('../assets/diceempty.png'), keep: false}
+                    {id: 0, diceValue: 0, diceImg: require('../assets/diceempty.png'), keep: false, activeDice: "inactiveDice"},
+                    {id: 1, diceValue: 0, diceImg: require('../assets/diceempty.png'), keep: false, activeDice: "inactiveDice"},
+                    {id: 2, diceValue: 0, diceImg: require('../assets/diceempty.png'), keep: false, activeDice: "inactiveDice"},
+                    {id: 3, diceValue: 0, diceImg: require('../assets/diceempty.png'), keep: false, activeDice: "inactiveDice"},
+                    {id: 4, diceValue: 0, diceImg: require('../assets/diceempty.png'), keep: false, activeDice: "inactiveDice"},
                 ],
                 player: 1,
                 tries: 3,
@@ -59,6 +54,15 @@
                     elt.diceImg = require('../assets/dice6.png')  
                 }
             },
+            keepDice(elt) {
+                if (elt.keep === false && elt.diceValue !==0){
+                    elt.keep = true
+                    elt.activeDice = "activeDice"
+                } else if (elt.keep === true || elt.diceValue === 0) {
+                    elt.keep = false
+                    elt.activeDice = "inactiveDice"
+                }
+            },
             nextPlayer() {
                 this.tries = 3
                 if (this.player === 1) {
@@ -70,6 +74,7 @@
                 for (let dice of this.dices) {
                     dice.diceValue = 0
                     dice.keep = false
+                    dice.activeDice = "inactiveDice"
                     this.showDice(dice)
                 }
                 this.disableNextPlayer = true
@@ -97,7 +102,16 @@
 <style>
 
 #dicesImages {
-    padding: 5px;
+
+}
+
+#dicesImages .inactiveDice {
+    margin: 12px;
+}
+
+#dicesImages .activeDice {
+    margin: 7px;
+    border: 5px solid green;
 }
 
 #rollButton {

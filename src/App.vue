599be196347bfbbf1a413e93adb3824e53a24de9
Aloïs Debande <63793809+adebande@users.noreply.github.com>
2020-04-26 22:19:19 +0200

    add dice component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="header">
+      <b-jumbotron fluid header="Yam's" lead="2 players game"></b-jumbotron>
+    </div>
+    <div id="dice">
+      <dice></dice>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import dice from './components/dice'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    dice
   }
 }
 </script>
 
 <style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
+
+#dice {
+  text-align: center;
+}
+
 </style>
---
+++ src/components/dice.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="dice">
+        <div id="diceImage">
+            <img :src="diceImg">
+        </div>
+        <div id="rollButton">
+            <b-button @click="roll">Roll dice</b-button>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'dice',
+        data() {
+            return {
+                diceValue: 6,
+                diceImg: require('../assets/dice6.png')
+            }
+        },
+        methods : {
+            showDice() {
+                if (this.diceValue === 1) {
+                    this.diceImg = require("../assets/dice1.png")
+                } else if (this.diceValue === 2) {
+                    this.diceImg = require('../assets/dice2.png')
+                } else if (this.diceValue === 3) {
+                    this.diceImg = require('../assets/dice3.png') 
+                } else if (this.diceValue === 4) {
+                    this.diceImg = require('../assets/dice4.png') 
+                } else if (this.diceValue === 5) {
+                    this.diceImg = require('../assets/dice5.png') 
+                } else if (this.diceValue === 6) {
+                    this.diceImg = require('../assets/dice6.png')  
+                }
+            },
+            roll() {
+                this.diceValue = Math.floor(Math.random() * 6) + 1
+                this.showDice()
+            },
+        },
+    }
+</script>
+
+<style>
+
+#diceImage {
+    padding: 5px;
+}
+
+#rollButton {
+    padding: 5px;
+}
+
+</style>
